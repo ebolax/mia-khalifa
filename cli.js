b8af601670581266d2595eb6b79cd8245d23ba45
ebolax <ebolax@gmail.com>
2021-11-22 13:25:33 +0300

    prettier

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
 
-const MiaKhalifa = require('./index');
+const MiaKhalifa = require("./index");
 
 MiaKhalifa.addToImage(process.argv[2], process.argv[3]);
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const MiaKhalifa = require('./index');
+const MiaKhalifa = require("./index");
 
-MiaKhalifa.addToImage('test/test1.jpg', 'test/test1_new.jpg');
-MiaKhalifa.addToImage('test/test2.png', 'test/test2_new.png');
-MiaKhalifa.addToImage('test/test3.jpg', 'test/test3_new.jpg');
+MiaKhalifa.addToImage("test/test1.jpg", "test/test1_new.jpg");
+MiaKhalifa.addToImage("test/test2.png", "test/test2_new.png");
+MiaKhalifa.addToImage("test/test3.jpg", "test/test3_new.jpg");
